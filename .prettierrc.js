fc4e762992932ae02972fe006fddb78b2bde3078
qiuzixu <2430251124@qq.com>
2024-02-01 10:50:55 +0800

    chore: 项目初始化

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  // useTabs: false,
+  semi: false, // 未尾逗号, default:  true
+  singleQuote: true, // 单引号 default: false
+  // quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all', // 未尾分号 default: es5    all | none | es5
+  // bracketSpacing: true,
+  // bracketSameLine: false,
+  // jsxBracketSameLine: false,
+  arrowParens: 'avoid', // default: always avoid
+  // insertPragma: false,
+  // requirePragma: false,
+  proseWrap: 'never',
+  // htmlWhitespaceSensitivity: 'css',
+  // vueIndentScriptAndStyle: false,  // .vue 缩进
+  endOfLine: 'auto', // default lf
+}
---
+++ vue3-components/components/SearchSelect/SearchSelect.vue
@@ -0,0 +1,119 @@
+<template>
+  <!-- <el-input v-model="val" size="small"></el-input> -->
+  <el-select
+    :style="`width: ${$props.width}`"
+    :value="value"
+    :filterable="$props.filterable"
+    v-bind="$attrs"
+    v-on="$listeners"
+    :clearable="$props.clearable"
+    @visible-change="handleClearOptions"
+    :filter-method="handleSelectFilter"
+    size="small"
+    :placeholder="$props.placeholder"
+    :disabled="$props.disabled"
+  >
+    <el-option
+      v-for="opt in optionsList"
+      :key="`${opt.dictValue || opt.value}-${opt.dictLabel || opt.label} `"
+      :label="opt.dictLabel || opt.label"
+      :value="opt.dictValue || opt.value"
+      :disabled="opt.disabled"
+    />
+  </el-select>
+</template>
+
+<script>
+import PinyinMatch from 'pinyin-match'
+import { ElSelect, ElOption, ElInput } from 'element-plus'
+import 'element-plus/es/components/select/style/css'
+
+export default {
+  name: 'SearchSelect',
+  components: { ElSelect, ElOption },
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    value: [String, Number, Array, Boolean],
+    filterable: {
+      type: Boolean,
+      default: true,
+    },
+    clearable: {
+      type: Boolean,
+      default: false,
+    },
+    options: {
+      type: Array,
+      default() {
+        return []
+      },
+      placeholder: {
+        type: String,
+        default: '请选择',
+      },
+    },
+    width: {
+      type: String,
+      default: '220px',
+    },
+    placeholder: {
+      type: String,
+      default: '请选择',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      optionsList: [],
+      copyOptionsList: [],
+    }
+  },
+  watch: {
+    options() {
+      this.initOptions()
+    },
+  },
+  created() {
+    this.initOptions()
+  },
+  methods: {
+    /**
+     * @Description: 下拉框支持模糊搜索
+     * @Author: Lucas
+     * @param {*} val
+     */
+    handleSelectFilter(val) {
+      try {
+        if (val) {
+          this.optionsList = this.copyOptionsList.filter(item =>
+            PinyinMatch.match(item.dictLabel || item.label, val),
+          )
+        } else {
+          this.optionsList = this.copyOptionsList
+        }
+      } catch (error) {
+        console.error('模糊音下拉框：', error)
+      }
+    },
+    /**
+     * @Description: 清除筛选恢复options
+     * @Author: Lucas
+     * @param {*} val 出现则为 true，隐藏则为 false
+     */
+    handleClearOptions(val) {
+      if (!val) return
+      this.optionsList = this.copyOptionsList
+    },
+    initOptions() {
+      this.optionsList = this.$props.options
+      this.copyOptionsList = JSON.parse(JSON.stringify(this.$props.options))
+    },
+  },
+}
+</script>
